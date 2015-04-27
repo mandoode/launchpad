261914e5d32a249d0b11196bbc698e77472143f0
mandoode <spamo@ziu.net>
2015-04-27 17:48:03 -0400

    added deploy for any app

---
+++ jenkins/deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -x
+
+APP_NAME="launchpad"
+
+CONTAINER_EXISTS="$(sudo docker ps -a | grep $APP_NAME)"
+if [ "$CONTAINER_EXISTS" == "0" ];then
+
+  CONTAINER_RUNNING="$(sudo docker ps | grep $APP_NAME)"
+  if [ "$CONTAINER_RUNNING" == "0" ];then
+	sudo docker stop $APP_NAME
+	if [ "$?" != "0" ];then
+		echo "could not stop running container $APP_NAME"
+		exit -1
+	else
+		sudo docker rm $APP_NAME
+		if [ "$?" != "0" ];then
+			echo "ERROR: could not remove container $APP_NAME" 	
+			exit -1
+		fi
+		exit -1
+	fi
+  fi
+fi
+
+sudo docker run -d --name $APP_NAME
+
+if [ "$?" != "0" ];then
+	echo "ERROR:  could not start container $APP_NAME"
+	exit -1
+fi
+
+echo "exiting..."
